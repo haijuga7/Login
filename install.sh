be64d0901ce605e40d5fffc14db874b20e6f5d94
Mr.A_S <36959705+haijuga7@users.noreply.github.com>
2018-09-17 03:46:46 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+read -p "Apakah Kamu Ingin Menggunakan Tampilan Ini (y/n) ? " ardi
+
+if [ $ardi == 'y' ] || [ $ardi == 'Y' ]
+then
+	echo "Tunggu Sebentar"
+	cd ..
+	mv -fin Login $HOME
+	cd Login
+	pkg install python2 -y &> /dev/null
+	mv -fin $HOME/../usr/etc/bash.bashrc /sdcard/Android &> /dev/null
+	mv -fin bash.bashrc $HOME/../usr/etc &> /dev/null
+	echo "Selesai"
+	echo "Tampilan Awal Kamu Disimpan Di /sdcard/Android"
+
+elif [ $ardi == 'n' ] || [ $ardi == 'N' ]
+then
+	echo "Keluar"
+	exit
+else
+	echo "Failed"
+	exit
+fi
